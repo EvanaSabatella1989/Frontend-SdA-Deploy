ff89af6a34273e00c233e99981caa704388a0853
EvanaSabatella1989 <eva.sabatella@gmail.com>
2026-01-08 11:23:57 -0300

    Validacion Stock carrito Frontend detalles

--- Angular/src/app/service/auth.interceptor.ts
+++ Angular/src/app/service/auth.interceptor.ts
@@ -43,7 +43,8 @@ export class AuthInterceptor implements HttpInterceptor {
       req.url.includes('/login') ||
       req.url.includes('/registro') ||
       req.url.includes('/home') ||
-      req.url.includes('/producto/') && !req.url.includes('/admin/')
+      req.url.includes('/producto/') ||
+      req.url.includes('/servicio/') && !req.url.includes('/admin/')
     ) {
       return next.handle(req);
     }
